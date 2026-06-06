effd8ca4a2e9ec05773b4f166948e31c0686787d
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-06-06 21:27:18 +0530

    Day-6 Uploaded.

---
+++ Day-6/Q23.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    cout << "Enter a number: ";
+    cin >> n;
+    while(n > 0) {
+        count += n % 2;
+        n /= 2;
+    }
+    cout << "Set Bits = " << count;
+    return 0;
+}

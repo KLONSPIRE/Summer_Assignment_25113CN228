d45283deadb2f16884f90b9610131cad09dcadb0
KLONSPIRE <abhishekkumarnishad1107@gmail.com>
2026-07-03 14:47:32 +0530

    Replace Q9.java with C++ solution

---
+++ Day-3/Q9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int prime(int n){
+    int i;
+    for(i = 2; i <= sqrt(n); i++)
+        if(n % i == 0)
+            break;
+    if(i > sqrt(n))
+        return n;
+    return 0;
+}
+
+int main(){
+    int n;
+    cout << "Enter a Number : ";
+    cin >> n;
+    if(prime(n) != 0)
+        cout << "Given No. is a Prime Number.";
+    else
+        cout << "Given No. is not a Prime Number.";
+    return 0;
+}
